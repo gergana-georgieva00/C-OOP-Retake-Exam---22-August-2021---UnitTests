dcba001c1b1704425049eb72698523bec4491cf9
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-09 12:24:13 +0200

    C# OOP Retake Exam - 22 August 2021

--- HeroRepository.Tests/HeroRepositoryTests.cs
+++ HeroRepository.Tests/HeroRepositoryTests.cs
@@ -3,5 +3,17 @@ using NUnit.Framework;
 
 public class HeroRepositoryTests
 {
-    
+    private HeroRepository heroRepository;
+
+    [SetUp]
+    public void SetUp()
+    {
+        heroRepository = new HeroRepository();
+    }
+
+    [Test]
+    public void CreateHeroWithNullHeroThrows()
+    {
+        Assert.Throws<ArgumentNullException>(() => heroRepository.Create(null));
+    }
 }
